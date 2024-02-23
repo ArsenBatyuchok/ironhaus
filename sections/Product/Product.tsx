dc6aa1767e47255b7191635ea562b0800155029e
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2024-02-23 10:49:03 +0100

    test UI

--- sections/Product/Product.tsx
+++ sections/Product/Product.tsx
@@ -1,28 +1,30 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 interface Props {
   productId: string;
 }
 
 export const Product: FC<Props> = ({ productId }) => {
+  const [wrapper, setWrapper] = useState<HTMLDivElement | null>(null);
   useEffect(() => {
-    if (!window.xProduct) return;
-    window.xProduct();
-  }, [productId]);
+    if (!wrapper) return;
+    window.xProduct?.();
+  }, [wrapper, productId]);
   return (
-  <div
-    className={`ecsp ecsp-SingleProduct-v2 ecsp-SingleProduct-v2-centered ecsp-Product ec-Product-${productId}`}
-    itemType="http://schema.org/Product"
-    data-single-product-id={productId}
-  >
-    {/* @ts-ignore */}
-    <div className="ecsp-title" itemProp="name" style={{ display: 'none' }}></div>
-    {/* @ts-ignore */}
-    <div customprop="options"></div>
-    {/* @ts-ignore */}
-    <div customprop="qty"></div>
-    {/* @ts-ignore */}
-    <div customprop="addtobag"></div>
-  </div>
+    <div
+      ref={setWrapper}
+      className={`ecsp ecsp-SingleProduct-v2 ecsp-SingleProduct-v2-centered ecsp-Product ec-Product-${productId}`}
+      itemType="http://schema.org/Product"
+      data-single-product-id={productId}
+    >
+      {/* @ts-ignore */}
+      <div className="ecsp-title" itemProp="name" style={{ display: 'none' }}></div>
+      {/* @ts-ignore */}
+      <div customprop="options"></div>
+      {/* @ts-ignore */}
+      <div customprop="qty"></div>
+      {/* @ts-ignore */}
+      <div customprop="addtobag"></div>
+    </div>
   );
 };
